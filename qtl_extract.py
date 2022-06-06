ad036227521d00dff5bca08f3c5a75a3e789005a
Ryan D. Ridenbaugh <92115812+RDRidenbaugh@users.noreply.github.com>
2022-06-06 10:20:14 -0400

    Add files via upload

--- qtl_extract.py
+++ qtl_extract.py
@@ -1,15 +1,21 @@
+# v.2.0
+
 gff = []
 def gff_read(path):
     with open(path, "r") as f:
         for line in f.readlines():
             if line.startswith("NC"):
                 temp_split  = line.split()
-                if temp_split[2] == "gene":
+                if temp_split[2] == "CDS":
                     gff.append(line.split())
 
+
 qtl = []
-scaffolds = {}
+genome = {}
+peak_genes = {}
 c_key = {"1":"NC_060260.1", "2":"NC_060261.1", "3":"NC_060262.1", "4":"NC_060263.1", "5":"NC_060264.1", "6":"NC_060265.1", "7":"NC_060266.1"}
+def compare_range(start1, end1, start2, end2):
+    return max(max((end2-start1), 0) - max((end2-end1), 0) - max((start2-start1), 0), 0)
 def qtl_extract(path_a, path_b, path_c):
     with open(path_a, "r") as f:
         for line in f.readlines():
@@ -26,22 +32,30 @@ def qtl_extract(path_a, path_b, path_c):
                 temp_list.append([temp_fasta, []])
             else: 
                 temp_list[-1][1].append(line)
-        scaffolds = {id: ''.join(seq).upper().replace('\n', '') for id, seq in temp_list}
-    output = open(path_c, "w")
+        genome = {id: ''.join(seq).upper().replace('\n', '') for id, seq in temp_list}
     for list_a in qtl:
         for list_b in gff:
             if c_key[list_a[1]] == list_b[0]:
-                range_a = range(int(list_a[2]), int(list_a[3]))
-                range_b = range(int(list_b[3]), int(list_b[4]))
-                overlap = [i for i in range_a if i in range_b]
-                if overlap:
-                    start = int(list_b[3])-1
-                    stop = int(list_b[4])-1
-                    temp_substring = scaffolds[list_b[0]][start:stop]
-                    output.write(">"+list_a[0]+" "+start+"-"+stop+" "+list_b[-1]+"\n"+temp_substring+"\n")
-                else:
-                    pass    
+                overlap = compare_range(int(list_a[2]), int(list_a[3]), int(list_b[3]), int(list_b[4]))
+                if overlap != 0:
+                    if list_a[0] not in peak_genes.keys():
+                        peak_genes[list_a[0]] = set()
+                        temp_split = list_b[8].split(";")
+                        temp_split = temp_split[0].split("-")
+                        peak_genes[list_a[0]] = set()
+                        peak_genes[list_a[0]].add(temp_split[-1])
+                    else:
+                        temp_split = list_b[8].split(";")
+                        temp_split = temp_split[0].split("-")
+                        peak_genes[list_a[0]].add(temp_split[-1])
+    output = open(path_c, "w")
+    for key in peak_genes.keys():
+        for transcript in peak_genes[key]:
+            output.write(">"+transcript+" "+key+"\n"+genome[transcript]+"\n")
     output.close()
 
-gff_read("../non_model/GCF_021901455.1_iyNeoLeco1.1_genomic.gff")
-qtl_extract("../non_model/color_qtl_loc.txt", "../non_model/GCF_021901455.1_iyNeoLeco1.1_genomic.fna", "../non_model/color_qtl_genes.fasta")
+gff_read("GCF_021901455.1_iyNeoLeco1.1_genomic.gff")
+qtl_extract("color_qtl_loc.txt", "GCF_021901455.1_iyNeoLeco1.1_protein.faa", "color_qtl_genes.faa")
+
+# gff_read("../non_model/GCF_021901455.1_iyNeoLeco1.1_genomic.gff")
+# qtl_extract("../non_model/color_qtl_loc.txt", "../non_model/GCF_021901455.1_iyNeoLeco1.1_protein.faa", "../non_model/color_qtl_genes.faa")
